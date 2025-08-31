d790d55cab46d3dd73337bd9591430a9ff4e3f3a
Bhavika Sainani <123266679+BhavikaSainani@users.noreply.github.com>
2025-08-31 10:58:59 +0530

    Delete utils directory

--- utils/aiValidator.js
+++
@@ -1,6 +0,0 @@
-module.exports.analyze = (photoUrl) => {
-    // Fake AI validation logic for hackathon demo
-    // Later can connect to real ML model
-    if (photoUrl.includes("mangrove")) return 0.9;
-    return 0.4;
-};
