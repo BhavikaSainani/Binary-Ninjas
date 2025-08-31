1ced3782152b363d6ae79e8172a6098698bd4c27
Bhavika Sainani <123266679+BhavikaSainani@users.noreply.github.com>
2025-08-31 10:55:34 +0530

    Delete models directory

--- models/Photo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const photoSchema = new mongoose.Schema({
-    url: { type: String, required: true },
-    validated: { type: Boolean, default: false },
-    uploadedAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Photo", photoSchema);
